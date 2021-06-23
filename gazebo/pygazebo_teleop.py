5ee341e2dacdb1e6863c07a1dff85b850769e449
dusty-nv <dustinf@nvidia.com>
2021-06-23 14:04:46 -0400

    updated teleop script

--- gazebo/pygazebo_teleop.py
+++ gazebo/pygazebo_teleop.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
+import signal
 import argparse
 import asyncio
 import pygazebo
 
 import numpy as np
+from PIL import Image
+
+from pynput import keyboard
 
 '''
 gz topic -l
 gz joint -m 'simple_diff' -j right_wheel_hinge --vel-t 0
 gz world --reset-all
+
+renice -n 15 $(pgrep gzclient)
 '''
 
 parser = argparse.ArgumentParser()
@@ -19,15 +26,19 @@ parser.add_argument('--host', default='localhost', type=str)
 parser.add_argument('--port', default=11346, type=int)
 parser.add_argument('--retry', default=30, type=int)
 
-parser.add_argument('--model', default='simple_diff', type=str)
+parser.add_argument('--robot', default='simple_diff', type=str)
 parser.add_argument('--camera', default='camera_link', type=str)
 parser.add_argument('--left-wheel', default='left_wheel_hinge', type=str)
 parser.add_argument('--right-wheel', default='right_wheel_hinge', type=str)
+parser.add_argument('--max-speed', default=2.0, type=float)
 
 args = parser.parse_args()
 print(args)
 
 
+#
+# gazebo connection
+#
 def gazebo_connect(host='localhost', port=11346, retry=30):
     async def _connect(host, port, retry):
         connected = False
@@ -81,41 +92,113 @@ for topic in manager.publications():
 
 # subscribe to topics
 def on_image(data):
-    print('image message:')
     msg = pygazebo.msg.image_stamped_pb2.ImageStamped()
     msg.ParseFromString(data)
-    print(msg.time)
-    print(f'width={msg.image.width} height={msg.image.height} pixel_format={msg.image.pixel_format} step={msg.image.step}')
     
     img = np.frombuffer(msg.image.data, dtype=np.uint8)
     img = np.reshape(img, (msg.image.height, msg.image.width, 3))
     
-    print(img.shape)
-    print('')
+    #print(msg.time)
+    #print(f'width={msg.image.width} height={msg.image.height} pixel_format={msg.image.pixel_format} step={msg.image.step}')
+    #print(img.shape)
+    #print('')
+    #Image.fromarray(img).save('data/test_image.jpg')
 
-image_subscriber = gazebo_subscribe(manager, f'/gazebo/default/{args.model}/{args.camera}/camera/image', 'gazebo.msgs.ImageStamped', on_image)
+image_subscriber = gazebo_subscribe(manager, f'/gazebo/default/{args.robot}/{args.camera}/camera/image', 'gazebo.msgs.ImageStamped', on_image)
  
 # advertise topics
-joint_publisher = gazebo_advertise(manager, f'/gazebo/default/{args.model}/joint_cmd', 'gazebo.msgs.JointCmd')
+joint_publisher = gazebo_advertise(manager, f'/gazebo/default/{args.robot}/joint_cmd', 'gazebo.msgs.JointCmd')
+
+    
+#
+# keyboard handler
+#
+key_states = {}
+
+def on_press(key):
+    global key_states
+
+    try:
+        key_states[key.char] = True
+    except AttributeError:
+        key_states[key] = True
+
+
+def on_release(key):
+    global key_states
+
+    try:
+        key_states[key.char] = False
+    except AttributeError:
+        key_states[key] = False
+
+keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+keyboard_listener.start()
+
+
+#
+# robot control
+#
+wheel_speed = {'left': None, 'right': None}
 
 def set_wheel_speed(left, right):
-    left_msg = pygazebo.msg.joint_cmd_pb2.JointCmd()
-    left_msg.name = f'{args.model}::{args.left_wheel}'
-    left_msg.velocity.target = left
+    global wheel_speed
+    changed_speed = False
     
-    right_msg = pygazebo.msg.joint_cmd_pb2.JointCmd()
-    right_msg.name = f'{args.model}::{args.right_wheel}'
-    right_msg.velocity.target = right
+    if wheel_speed['left'] != left:
+        left_msg = pygazebo.msg.joint_cmd_pb2.JointCmd()
+        left_msg.name = f'{args.robot}::{args.left_wheel}'
+        left_msg.velocity.target = left
+        joint_publisher.publish(left_msg)
+        wheel_speed['left'] = left
+        changed_speed = True
+        
+    if wheel_speed['right'] != right:
+        right_msg = pygazebo.msg.joint_cmd_pb2.JointCmd()
+        right_msg.name = f'{args.robot}::{args.right_wheel}'
+        right_msg.velocity.target = right
+        joint_publisher.publish(right_msg)
+        wheel_speed['right'] = right
+        changed_speed = True
+        
+    if changed_speed:
+        print(f"set_wheel_speed({left}, {right})")
     
-    joint_publisher.publish(left_msg)
-    joint_publisher.publish(right_msg)
-    
+def teleop(max_speed=1.0):
+    if key_states.get(keyboard.Key.left) or key_states.get('a'):
+        set_wheel_speed(-max_speed, max_speed)
+    elif key_states.get(keyboard.Key.right) or key_states.get('d'):
+        set_wheel_speed(max_speed, -max_speed)
+    elif key_states.get(keyboard.Key.up) or key_states.get('w'):
+        set_wheel_speed(max_speed, max_speed)
+    elif key_states.get(keyboard.Key.down) or key_states.get('s'):
+        set_wheel_speed(-max_speed, -max_speed)
+    else:
+        set_wheel_speed(0,0)
+        
+
+#
+# main loop
+#
+run_signal = True
+
+def signal_handler(sig, frame):
+    global run_signal
+    run_signal = False
+    print('Ctrl+C pressed, exiting...')
+
+signal.signal(signal.SIGINT, signal_handler)
+
 # main loop
 async def run():
-    while True:
-        set_wheel_speed(0.0, 0.0)
+    while run_signal:
+        teleop(args.max_speed)
         await asyncio.sleep(0.1)
  
+    print('Shutting down, stopping robot...')
+    set_wheel_speed(0,0)
+    await asyncio.sleep(1.0)
+    
 asyncio.get_event_loop().run_until_complete(run())
 
     
