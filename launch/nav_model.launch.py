8163ff5aa24a35208f976509cac584faaee9c1c3
Dustin Franklin <dustinf@nvidia.com>
2021-07-16 16:47:44 -0400

    updated launch files

--- launch/nav_model.launch.py
+++ launch/nav_model.launch.py
@@ -11,9 +11,14 @@ def generate_launch_description():
     
     model_arg = DeclareLaunchArgument('model')
     
+    speed_gain = DeclareLaunchArgument('speed_gain', default_value="0.15")
+    steering_gain = DeclareLaunchArgument('steering_gain', default_value="0.4")
+    
     nav_model = Node(package='jetbot_ros', node_executable='nav_model',
                      parameters=[
                         {"model": LaunchConfiguration('model')},
+                        {"speed_gain": LaunchConfiguration('speed_gain')},
+                        {"steering_gain": LaunchConfiguration('steering_gain')},
                         {"visualize": True},
                      ],
                      output='screen',
@@ -23,5 +28,7 @@ def generate_launch_description():
                      
     return LaunchDescription([
         model_arg,
+        speed_gain,
+        steering_gain,
         nav_model
     ])
